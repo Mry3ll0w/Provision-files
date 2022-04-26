ac84d40b2471f0117105745e3ea50dd7c0025bc9
ara <ara@ara.localdomain>
2022-04-26 16:57:28 +0200

    nombre cambio

--- provision_poo.sh
+++ provision_poo.sh
@@ -2,8 +2,10 @@
 
 #REQUISISTO ==> SER ROOT
 echo "----------------------------------------------KIT PARA POO ---------------------------------------"
+echo "                       "
 echo "Recuerda que para que se ejecute de forma correcta el provision tienes que ser root!!"
 echo "Se instalaran los siguientes paquetes (Usando ubuntu no es necesario meter repos):"
+echo "              "
 echo "binutils g++ cowsay neofetch clang valgrind language-pack-es"
 
 apt install -y binutils g++ cowsay neofetch clang valgrind language-pack-es
