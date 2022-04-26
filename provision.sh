d202475f8375e730f3c6afec4f4a834a5fb79369
ara <ara@ara.localdomain>
2022-04-26 16:53:52 +0200

    Primer Provision de POO

---
+++ provision.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#REQUISISTO ==> SER ROOT
+echo "----------------------------------------------KIT PARA POO ---------------------------------------"
+echo "Recuerda que para que se ejecute de forma correcta el provision tienes que ser root!!"
+echo "Se instalaran los siguientes paquetes (Usando ubuntu no es necesario meter repos):"
+echo "binutils g++ cowsay neofetch clang valgrind language-pack-es"
+
+apt install -y binutils g++ cowsay neofetch clang valgrind language-pack-es
